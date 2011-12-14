a7d4cef42851e4da3459572788a8ceafc6b8ad88
Zhaojun Zhang <zhangzhaojun@gmail.com>
2011-12-14 13:50:52 -0500

    detect allele specific isoforms

---
+++ graph/path.cpp
@@ -0,0 +1,65 @@
+#include "path.hpp"
+
+Path::Path(vector<ExonNode*> &current_path){
+	path=current_path;
+}
+
+
+bool Path::is_mirrored(Path &a){
+	// must be informative path (at least contain one allele specific
+	// exon)
+
+	bool ret=false;
+
+	for (int i=0;i<path.size();i++){
+		if (!path[i]->is_mirrored(a.path[i])){
+			return false;
+		}
+		if (path[i]->origin!=EXON_NO_INFO){
+			ret=true;
+		}
+	}
+	return ret;
+}
+
+bool Path::is_informative(){
+	for (int i=0;i<path.size();i++){
+		if (path[i]->origin!=EXON_NO_INFO){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Path::is_equal(Path &a){
+	if (a.path.size()!=path.size()){
+		return false;
+	}
+	for (int i=0;i<path.size();i++){
+		if (!path[i]->is_equal(a.path[i])){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int Path::dump_path(FILE * file){
+	int i;
+	if (is_valid()){
+		fprintf(file,"%s ",path[0]->detach().c_str());
+		for (i=1;i<path.size();i++){
+			fprintf(file,"-> %s ",path[i]->detach().c_str());
+		}
+		fprintf(file,"\n");
+	}
+}
+
+bool Path::is_valid(){
+	for (int i=0;i<path.size();i++){
+		if (path[i]==NULL){
+			return false;
+		}
+	}
+	return true;
+}
---
+++ graph/path.hpp
@@ -0,0 +1,18 @@
+#ifndef _PATH_H_
+#define _PATH_H_
+
+#include "exon_node.hpp"
+class Path{
+public:
+	Path(vector<ExonNode *> &);
+	vector<ExonNode *> path;
+	bool is_mirrored(Path &path);
+	bool is_equal(Path &path);
+int dump_path(FILE *file);
+// contains allele specific exons or not
+bool is_informative();
+// contains NULL pointer or not
+bool is_valid();
+
+};
+#endif
