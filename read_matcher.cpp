a83a0bf7858cc73bfb06fde7ce8b8ec84e53b9f3
Zhaojun Zhang <zhangzhaojun@gmail.com>
2012-02-28 00:55:59 -0500

    add read_matcher class to store match_allele results

---
+++ read_matcher.cpp
@@ -0,0 +1,19 @@
+
+
+#include "read_matcher.hpp"
+
+
+void ReadMatcher::output(JewelerAlignment *al){
+	int i;
+	if (mismatch_read_locations.size() == 0 ) return ;
+	for ( i = 0; i < mismatch_read_locations.size(); i++){
+		fprintf(stdout, "%d\t", al->read_position[mismatch_read_locations[i]]);
+	}
+	for ( i = 0; i < mismatch_read_locations.size(); i++){
+		fprintf(stdout, "%d\t", mismatch_read_locations[i]);
+	}
+	for ( i = 0; i < mismatch_transcript_locations.size(); i++){
+		fprintf(stdout, "%d\t", mismatch_transcript_locations[i]);
+	}
+	fprintf(stdout, "\n");
+}
---
+++ read_matcher.hpp
@@ -0,0 +1,22 @@
+#ifndef _READ_MATCHER_CPP_
+#define _READ_MATCHER_CPP_
+#include <vector> 
+#include "jeweler_alignment.hpp"
+
+using namespace std;
+
+class ReadMatcher {
+public:
+	vector<int> mismatch_read_locations;
+	vector<int> mismatch_transcript_locations;
+	vector<int> allele_read_locations;
+	vector<int> allele_transcript_locations;
+	vector<char> mismatchars;
+	vector<char> mismatch_qualities;
+	vector<int> transcript_aligned_locations;
+	vector<int> read_aligned_locations;
+	int num_matched_alleles;
+	void output(JewelerAlignment *al);
+};
+
+#endif //  _READ_MISMATCHER_CPP_
