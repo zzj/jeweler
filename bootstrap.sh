de05b4dee876d5ab81df4318d2262680e0aee85e
Zhaojun Zhang <zhangzhaojun@gmail.com>
2013-02-11 12:44:54 -0500

    move bootstrap folders form HOME to local`

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,17 @@ wget http://downloads.sourceforge.net/project/boost/boost/1.52.0/boost_1_52_0.ta
 tar zxf boost_1_52_0.tar.gz -C lib/
 cd lib/boost_1_52_0
 ./bootstrap.sh
-./b2 install --prefix=$HOME/bin/
+./b2 install --prefix=`pwd`/../../third_party/bin/
 
 cd ../leveldb-1.5.0
 make
 
 cd ../glog-0.3.2/
-./configure --prefix=$HOME/bin/
+./configure --prefix=`pwd`/../../third_party/bin/
 make
 
 cd ../protobuf-2.4.1
-./configure --prefix=$HOME/bin/
+./configure --prefix=`pwd`/../../third_party/bin/
 make
 make install
 
